30bbc311418a6604d0ae4b34dfc170ac1cfafcd8
machinea18 <109594561+machinea18@users.noreply.github.com>
2025-09-25 15:44:28 +0100

    Add files via upload

---
+++ step4_LPISprocessing.R
@@ -0,0 +1,26 @@
+#loading relevant packages
+library(terra)
+library(parallel) #package for parallel processing
+
+#files, please input oaths manually
+parcels <- vect("PATH1") #LPIS parcels
+TCD10m <- rast("PATH4") #copernicus TCD layer, likely VRT
+numcores <- 1 #change this to reflect the number of cores you will be using
+
+
+
+parcel_processing <- function(parcels, x) {
+  #using the zonal function to get the weighted mean of tree cover
+  #weights means that the fraction of the overlap in the polygon is also relevant
+  #so if a pixels only overlaps 50% its weight will only be 50%
+  return(zonal(TCD10m, parcels[x,], weights = TRUE))
+}
+
+#note this code does not work on a windows computer. It will run with only one core
+results <- mclapply(X = 1:length(parcels), FUN = parcel_processing,
+                           parcels = parcels,
+                          mc.cores = numcores)
+results<- unlist(results)
+
+write.csv(results, "FILEANME.csv") #please change filename 
+#this outputs a one column CSV file that can then be attached back to the LPIS data
