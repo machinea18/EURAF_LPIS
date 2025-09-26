983a92cbcc612720e0bdce3c356a21372e82f299
machinea18 <109594561+machinea18@users.noreply.github.com>
2025-09-26 13:50:33 +0100

    Add files via upload
    Download TCD10m using the Wekeo client. Note, login details have been redacted so please enter your own.

---
+++ TCD10m_downloading.R
@@ -0,0 +1,81 @@
+#downloading the files
+library(hdar)
+
+
+#this code extracts downloaded Copernicus files and unzips them
+#changeoutdir and files to match your computer
+#downloading into my downloads
+#getting login details
+username <- "REDACTED"
+password <-"REDACTED"
+client <- Client$new(username, password)
+client$get_token() #checking that this is successful
+
+#downloading files
+query <- '{
+  "dataset_id": "EO:EEA:DAT:HRL:TCF",
+  "productType": "Tree Cover Density",
+  "resolution": "10m",
+  "year": "2021",
+  "startIndex": 1
+}'
+
+#searching data
+matches <- client$search(query)
+matches_list <- matches$results
+matches_names <- rep(NA, 850)
+for(i in 1:850) {
+  matches_names[i] <- matches_list[[i]][["id"]]
+} #getting the names of the files
+
+#total of 850 files with 21GB (zipped)
+#downloading the files
+matches$download("//uol.le.ac.uk/root/staff/home/a/am1355/Downloads/",
+                 force = FALSE,
+                 prompt = FALSE,
+                 selected_indexes = 1:850,
+                 stop_at_failure = FALSE)
+#need to redownload some check which are missing
+files <- list.files("//uol.le.ac.uk/root/staff/home/a/am1355/Downloads/",
+                    pattern = "R00.zip")
+files_included <- sub('\\.zip$', '', files)
+missing <- which(!(matches_names %in% files_included))
+#give a total of 99 files, which seems about right
+matches$download("//uol.le.ac.uk/root/staff/home/a/am1355/Downloads/",
+                 force = FALSE,
+                 prompt = FALSE,
+                 selected_indexes = missing,
+                 stop_at_failure = FALSE)
+#now we have downloaded them all!
+
+
+#unzipping the files
+outDir<-"C:/Users/am1355/OneDrive - University of Leicester/Publications/EURAF_LPIS/TCD10m_raw/"
+#getting files
+files <- list.files("//uol.le.ac.uk/root/staff/home/a/am1355/Downloads/",
+                    pattern = "R00.zip")
+for(i in 1:length(files)) {
+  tif_file <- paste(sub('\\.zip$', '', files[i]) ,
+                     ".tif", sep = "")
+#extracting
+  unzip(zipfile = paste("//uol.le.ac.uk/root/staff/home/a/am1355/Downloads/",
+                        files[i], sep = ""),
+        files = tif_file, list = FALSE, overwrite = TRUE,
+        exdir = outDir, unzip = "internal")
+}
+
+#vrt making script
+library(terra)
+
+files <- list.files("C:/Users/am1355/OneDrive - University of Leicester/Publications/EURAF_LPIS/TCD10m_raw/",
+                    full.names = TRUE)
+
+vrt(files, filename = "C:/Users/am1355/OneDrive - University of Leicester/Publications/EURAF_LPIS/TCD10m")
+
+#testing that everything looks fine
+TCD10m <- rast("C:/Users/am1355/OneDrive - University of Leicester/Publications/EURAF_LPIS/TCD10m")
+
+#plotting
+plot(TCD10m)
+
+#amazing looks great!
